e222cfe579290a485b1eb896c7da1d70d3a901f8
pedrombafonso <mail@pedroafonso.net>
2013-12-12 12:21:55 +0000

    working with credentials.js, have to fix outdated auth_token

--- tests/test.js
+++ tests/test.js
@@ -10,35 +10,43 @@ var email = credentials.email;
 var password = credentials.password;
 
 test('Test Job', function(t){
-
   var programString = "function Run(data){ return data *2;}";
   var bid = 1;
 
   crowdProcess(programString, bid, undefined, email, password, function(err, job){
 
-    //Send data units
-    var numDataUnits = 10;
-    for (var i = 1; i <= numDataUnits; i++) {
+    // Input stream (create tasks)
+    var nTasks = 10;
+    for (var i = 1; i <= nTasks; i++) {
       console.log('-->send:', i)
       job.write(i);
     };
+    job.end();
 
-    var sumReceived = 0;
-    //Deal with results
-    job.on('data', function(result){
-      dealWithResults(result);
-    });
+    var rcvdTotal = 0,
+        rcvdErrors = 0,
+        rcvdResults = 0;
 
-    var received = 0
-    function dealWithResults(result){
-      console.log('-->result:', received+1);
-      sumReceived += result;
-      if (++received === numDataUnits){
-        job.end();
-        t.ok(sumReceived === numDataUnits*(numDataUnits+1), 'Everything is ok.');
+    // Output stream (receive results and errors)
+    job.on('data' , handleResult);
+    job.on('error', handleError );
+
+    function handleResult (result) {
+      rcvdResults++;
+      rcvdTotal = rcvdErrors + rcvdResults;
+      console.log(' > result:', rcvdResults);
+      if (rcvdTotal === nTasks){
+        job.destroy();
+        t.ok(rcvdTotal === nTasks, 'Received errors and results equals number of tasks.');
         t.end();
       }
     }
-  });
+    
+    function handleError (error) {
+      rcvdErrors++;
+      console.warn(' > error:', rcvdErrors);
+    }
 
+  });
 });
+
