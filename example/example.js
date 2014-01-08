25782710b7c363b865b35c6603c50e2f253f0d8a
Flávio Sousa <flavio.jsousa@gmail.com>
2014-01-08 14:51:12 +0000

    require credentials

--- example/example.js
+++ example/example.js
@@ -1,12 +1,13 @@
 var CrowdProcess = require('..');
 
 function Run(d) {
-  return d;
+  return d*2;
 }
 
 var data = [1, 2, 3];
 
-var crp = new CrowdProcess('email@example.com', 'password');
+var credentials = require('../credentials');
+var crp = new CrowdProcess(credentials.email, credentials.password);
 
 crp.map(Run, data, function(err, result) {
   console.log(result);
